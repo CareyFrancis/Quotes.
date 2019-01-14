7d9a6dbbcd4dcf81a25adf9da5bb709511d50232
CareyFrancis <carexfm@gmail.com>
2019-01-14 15:47:02 +0300

    Created the quotes details folder

---
+++ src/app/quotes-details/quotes-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes-details',
+  templateUrl: './quotes-details.component.html',
+  styleUrls: ['./quotes-details.component.css']
+})
+export class QuotesDetailsComponent implements OnInit {
+
+  @Input() quote: Quote;
+  @
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
