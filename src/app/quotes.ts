b53cf3c9b0c882998b0337aad0dab4a717c095fd
CareyFrancis <carexfm@gmail.com>
2019-01-14 12:31:40 +0300

    Updated the content of the quotes.ts file

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,3 @@
-export class Quotes {
-}
+export class Quote {
+public showDescription:boolean;
+  constructor (public id: number,  public author: string, public quote: string, public submitter: string,public upvotes: number, public downvotes: number,public totalVotes: number)
