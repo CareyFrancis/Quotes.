01b0c77e5f028a1dfae56b8014a490e95dde9864
CareyFrancis <carexfm@gmail.com>
2019-01-14 12:52:27 +0300

    Added quotes to the quotes component ts files

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-
+  Quotes= [
+    new Quote(1,'Carey Francis', 'Learning Advanced Mathematics is easy,said no one ever!'),
+    new Quote(2,'Kendrick Lamar','If God got us ,then we are going to be alright!'),
+    new Quote(3,'Robert Oppenheimer','Now I am become death, the destroyer of wolrds.'),
+    new Quote(4,'Albert Einstein','Imagination is more important than knowledge.'),
+    new Quote(5,'Donald Trump','I would give myself an A+'),
+  ]
   constructor() { }
 
   ngOnInit() {
